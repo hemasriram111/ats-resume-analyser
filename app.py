34dae9e5d9022b56a59cd4d0db0df6a6d3509493
hemasriram111 <hemasriram111@gmail.com>
2024-08-16 12:03:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+import PyPDF2 as pdf
+from dotenv import load_dotenv
+import json
+
+load_dotenv()  ## load all our environment variables
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(input)
+    return response.text
+
+def input_pdf_text(uploaded_file):
+    reader = pdf.PdfReader(uploaded_file)
+    text = ""
+    for page in range(len(reader.pages)):
+        page = reader.pages[page]
+        text += str(page.extract_text())
+    return text
+
+# Prompt Template
+
+input_prompt = """
+Hey Act Like a skilled or very experience ATS(Application Tracking System)
+with a deep understanding of tech field,software engineering,data science ,data analyst
+and big data engineer. Your task is to evaluate the resume based on the given job description.
+You must consider the job market is very competitive and you should provide 
+best assistance for improving thr resumes. Assign the percentage Matching based 
+on Jd and
+the missing keywords with high accuracy
+resume:{text}
+description:{jd}
+
+I want the response in one single string having the structure
+{{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
+"""
+
+## streamlit app
+st.title("Smart ATS")
+st.text("Improve Your Resume ATS")
+jd = st.text_area("Paste the Job Description")
+uploaded_file = st.file_uploader(
+    "Upload Your Resume", type="pdf", help="Please upload the pdf"
+)
+
+submit = st.button("Submit")
+
+if submit:
+    if uploaded_file is not None:
+        text = input_pdf_text(uploaded_file)
+        response = get_gemini_response(input_prompt.format(text=text, jd=jd))
+        try:
+            response_json = json.loads(response)
+            st.header("Resume Analysis")
+            st.write("")
+            
+            # Missing Keywords
+            st.subheader("Missing Keywords")
+            missing_keywords = response_json["MissingKeywords"]
+            st.write("• " + "\n• ".join(sorted(missing_keywords)))
+
+            # About the Resume
+            st.subheader("About the Resume")
+            profile_summary = response_json["Profile Summary"]
+            candidate_skills = [skill.strip() for skill in profile_summary.split(",")]
+            st.write("• " + "\n• ".join(sorted(candidate_skills)))
+
+            # Percentage Match
+            st.subheader("Percentage Match")
+            st.write(f"{response_json['JD Match']}%")
+            
+            st.write("")
+            st.write("")
+
+        except json.JSONDecodeError:
+            st.error("Error decoding response from Gemini.")
